63dfc6b1f75c8b975d4485e283c1075a6786900a
LeeChangHwan <changhwanlee94@gmail.com>
2022-06-10 23:41:17 +0900

    remove button component

--- src/components/button/button.jsx
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import styles from './button.module.css';
-
-const Button = ({ name }) => {
-  return <button className={styles.button}>{name}</button>;
-};
-
-export default Button;
