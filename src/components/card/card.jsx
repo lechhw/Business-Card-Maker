18392bab7671cbc3341e8d599efd6a0b771ec9ff
LeeChangHwan <changhwanlee94@gmail.com>
2022-06-13 17:04:41 +0900

    🐛 card 저장시 잘려서 저장되는 에러 해결

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -16,32 +16,34 @@ const Card = memo(({ card }) => {
       return card.tagName !== 'BUTTON';
     };
     domToImage.toBlob(card, { filter: filter }).then((blob) => {
-      saveAs(blob, 'card.png');
+      saveAs(blob, `💼 ${name} card.png`);
     });
   };
 
   return (
     <li className={styles.list}>
-      <article ref={cardRef} className={`${styles.card} ${getTheme(theme)}`}>
-        <button className={styles.save} onClick={onSaveFile}>
-          <img src="./images/save.png" alt="save" />
-        </button>
+      <div ref={cardRef}>
+        <article className={`${styles.card} ${getTheme(theme)}`}>
+          <button className={styles.save} onClick={onSaveFile} title="save">
+            <img src="./images/save.png" alt="save" />
+          </button>
 
-        <div className={styles.info}>
-          <div className={styles.profile}>
-            <img className={styles.avatar} src={url} alt="profile" />
+          <div className={styles.info}>
+            <div className={styles.profile}>
+              <img className={styles.avatar} src={url} alt="profile" />
+            </div>
+            <strong className={styles.name}>{name}</strong>
+            <p className={styles.title}>{title}</p>
           </div>
-          <strong className={styles.name}>{name}</strong>
-          <p className={styles.title}>{title}</p>
-        </div>
 
-        <div className={styles.detail_info}>
-          <p className={styles.company}>{company}</p>
-          <p className={styles.address}>{address}</p>
-          <p className={styles.number}>{number}</p>
-          <p className={styles.email}>{email}</p>
-        </div>
-      </article>
+          <div className={styles.detail_info}>
+            <p className={styles.company}>{company}</p>
+            <p className={styles.address}>{address}</p>
+            <p className={styles.number}>{number}</p>
+            <p className={styles.email}>{email}</p>
+          </div>
+        </article>
+      </div>
     </li>
   );
 });
