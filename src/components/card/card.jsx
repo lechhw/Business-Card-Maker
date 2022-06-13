6bfd1164797097ea65fa0935b42bd563ee0c2119
LeeChangHwan <changhwanlee94@gmail.com>
2022-06-13 16:48:39 +0900

    ✨ implement save file function

--- src/components/card/card.jsx
+++ src/components/card/card.jsx
@@ -1,15 +1,32 @@
-import React, { memo } from 'react';
+import React, { memo, useRef } from 'react';
 import styles from './card.module.css';
+import domToImage from 'dom-to-image';
+import { saveAs } from 'file-saver';
 
 const DEFAULT_IMAGE = './images/default_profile.png';
 
 const Card = memo(({ card }) => {
   const { name, title, company, address, number, email, theme, fileURL } = card;
   const url = fileURL || DEFAULT_IMAGE;
+  const cardRef = useRef();
+
+  const onSaveFile = () => {
+    const card = cardRef.current;
+    const filter = (card) => {
+      return card.tagName !== 'BUTTON';
+    };
+    domToImage.toBlob(card, { filter: filter }).then((blob) => {
+      saveAs(blob, 'card.png');
+    });
+  };
 
   return (
     <li className={styles.list}>
-      <article className={`${styles.card} ${getTheme(theme)}`}>
+      <article ref={cardRef} className={`${styles.card} ${getTheme(theme)}`}>
+        <button className={styles.save} onClick={onSaveFile}>
+          <img src="./images/save.png" alt="save" />
+        </button>
+
         <div className={styles.info}>
           <div className={styles.profile}>
             <img className={styles.avatar} src={url} alt="profile" />
@@ -17,6 +34,7 @@ const Card = memo(({ card }) => {
           <strong className={styles.name}>{name}</strong>
           <p className={styles.title}>{title}</p>
         </div>
+
         <div className={styles.detail_info}>
           <p className={styles.company}>{company}</p>
           <p className={styles.address}>{address}</p>
